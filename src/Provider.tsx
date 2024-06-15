72ef2332ff0aedae6c692c85890015f5d1ae7aeb
Tomáš Nývlt <tomas.nyvlt@o2.cz>
2024-06-15 21:32:23 +0200

    Update

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,25 +1,21 @@
 import { GTMProvider } from '@elgorditosalsero/react-gtm-hook';
 import { ThemeProvider } from '@xstyled/emotion';
-import { AnolisProvider, AnolisTheme } from 'anolis-ui';
+import { AnolisProvider } from 'anolis-ui';
 import { ReactNode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { theme } from './theme';
+import { customXstyledTheme } from './theme/xstyled';
 
 export interface ProviderProps {
-  customXstyledTheme: Parameters<typeof ThemeProvider>[0]['theme'];
-  anolisTheme: AnolisTheme;
   children: ReactNode;
 }
 
-export const Provider = ({
-  children,
-  customXstyledTheme,
-  anolisTheme,
-}: ProviderProps) => {
+export const Provider = ({ children }: ProviderProps) => {
   return (
     <BrowserRouter>
       <GTMProvider state={undefined}>
         <ThemeProvider theme={customXstyledTheme}>
-          <AnolisProvider theme={anolisTheme}>{children}</AnolisProvider>
+          <AnolisProvider theme={theme}>{children}</AnolisProvider>
         </ThemeProvider>
       </GTMProvider>
     </BrowserRouter>
